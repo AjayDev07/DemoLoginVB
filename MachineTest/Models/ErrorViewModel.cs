1a2256e16511b9b2af5601ccf7ae0e055f00a943
Ajay Shukla <141300540+AjayDev07@users.noreply.github.com>
2024-01-10 14:01:12 +0530

    Delete MachineTest directory

--- MachineTest/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MachineTest.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
