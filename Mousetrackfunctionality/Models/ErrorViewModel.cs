61b6a1b0d0a388bb68b50dd1a2b74ead9abeec2b
Ajay Shukla <141300540+AjayDev07@users.noreply.github.com>
2024-01-10 14:02:09 +0530

    Delete Mousetrackfunctionality directory

--- Mousetrackfunctionality/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Mousetrackfunctionality.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
